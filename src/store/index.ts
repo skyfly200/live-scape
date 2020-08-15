5eabfe97593bf1e419b936a74aa57436c1ee40b2
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-15 14:34:57 -0600

    add test data

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,41 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+      {
+        id: 0,
+        title: "Weed and Deadhead",
+        note: "Weed and deadhead the back and side yards",
+        tools: [0, 1, 2],
+        images: [],
+      },
+    ],
+    tools: [
+      {
+        name: "Weeding Tool",
+        image: "",
+        size: "small",
+        description: "",
+      },
+      {
+        name: "Hand Pruners",
+        image: "",
+        size: "small",
+        description: "",
+      },
+      {
+        name: "Bucket",
+        image: "",
+        size: "medium",
+        description: "",
+      },
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  mutations: {},
+  actions: {},
+  modules: {},
 });
