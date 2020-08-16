be9fbb321e04daf758eb2b600d5303b2c8b9346e
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-16 12:52:26 -0600

    add the Tasks page

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,49 @@
 <template lang="pug">
-  v-app#app
-    v-navigation-drawer(app two-line v-model="drawer")
-      v-list-item(link)
-        v-dialog(v-model="loginDialog" width="500")
-          template(v-slot:activator="{ on, attrs }")
-            v-list-item-avatar
-              img(src="https://randomuser.me/api/portraits/men/81.jpg")
-            v-list-item-content(v-bind="attrs" v-on="on")
-              v-list-item-title(text) Login or Register
-          Login
-      v-divider
-      v-list-item(link to="/" text)
-        v-list-item-icon
-          v-icon mdi-home
-        v-list-item-content
-          v-list-item-title Home
-    v-app-bar(app)#nav
-      v-app-bar-nav-icon(@click="drawer = true")
-      v-toolbar-title LiveScape
+v-app#app
+  v-navigation-drawer(app, two-line, v-model="drawer")
+    v-list-item(link)
+      v-dialog(v-model="loginDialog", width="500")
+        template(v-slot:activator="{ on, attrs }")
+          v-list-item-avatar
+            img(src="https://randomuser.me/api/portraits/men/81.jpg")
+          v-list-item-content(v-bind="attrs", v-on="on")
+            v-list-item-title(text) Login or Register
+        Login
+    v-divider
+    v-list-item(link, to="/", text)
+      v-list-item-icon
+        v-icon mdi-home
+      v-list-item-content
+        v-list-item-title Home
+    v-list-item(link, to="/tasks", text)
+      v-list-item-icon
+        v-icon mdi-home
+      v-list-item-content
+        v-list-item-title Tasks
+  v-app-bar#nav(app)
+    v-app-bar-nav-icon(@click="drawer = true")
+    v-toolbar-title LiveScape
+    v-spacer
+    v-text-field(
+      solo-inverted,
+      flat,
+      hide-details,
+      v-model="query",
+      label="Search",
+      prepend-inner-icon="mdi-magnify"
+    )
+    .largeMenu
       v-spacer
-      v-text-field(solo-inverted
-        flat
-        hide-details
-        v-model="query"
-        label="Search"
-        prepend-inner-icon="mdi-magnify")
-      .largeMenu
-        v-spacer
-        v-btn(to="/" text) Home
-        v-btn(@click.stop="loginDialog = true" text) Login
-        v-btn(icon)
-          v-icon mdi-help
-    v-main
-      v-container(fluid)
-        router-view
-    v-footer(app)
-      p Copyright Polytope Solutions 2020
+      v-btn(to="/", text) Home
+      v-btn(to="/tasks", text) Tasks
+      v-btn(@click.stop="loginDialog = true", text) Login
+      v-btn(icon)
+        v-icon mdi-help
+  v-main
+    v-container(fluid)
+      router-view
+  v-footer(app)
+    p Copyright Polytope Solutions 2020
 </template>
 
 <script lang="ts">
---
+++ src/views/Tasks.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+v-container.tasks(fluid)
+  v-card.ma-2(v-for="task in tasks", width="400")
+    v-card-title {{ task.title }}
+    v-card-subtitle {{ task.description }}
+    v-card-text
+      h4 Customer: {{ locations[task.location].customerName }}
+      h4 Address: {{ locations[task.location].address }}
+      p {{ task.notes }}
+      h3 Tools
+      v-list
+        v-list-item(v-for="t in task.tools", two-line)
+          v-list-item-avatar(v-if="tools[t].image")
+            v-img(:src="tools[t].image")
+          v-list-item-content
+            v-list-item-title {{ tools[t].name }}
+            v-list-item-subtitle {{ tools[t].description }}
+      v-img(v-for="i in task.images", width="50", src="i")
+    v-card-actions
+      v-btn Start
+</template>
+
+<script>
+export default {
+  name: "Tasks",
+  data: () => ({
+    tasks: [
+      {
+        id: 0,
+        location: 0,
+        title: "Weed and Deadhead",
+        description: "Weed and deadhead the back and side yards",
+        notes: "",
+        tools: [0, 1, 2],
+        images: [],
+      },
+      {
+        id: 1,
+        location: 1,
+        title: "Plant Suculants",
+        description: "Plant the suculants in the garden",
+        notes: "",
+        tools: [0, 1, 2],
+        images: [],
+      },
+    ],
+    locations: [
+      {
+        customerName: "Libby Fly",
+        address: "2644 Mapleton Ave. Boulder, CO",
+        notes: "Close the gate",
+      },
+      {
+        customerName: "Jerry Mathews",
+        address: "1234 Bluff St. Boulder, CO",
+        notes: "Don't run the blower",
+      },
+    ],
+    tools: [
+      {
+        name: "Weeding Tool",
+        image:
+          "https://www.gardenersedge.com/images/500/4752.jpg?v=100000227430-2",
+        size: "small",
+        description: "For pulling up weeds at the root",
+      },
+      {
+        name: "Hand Pruners",
+        image:
+          "https://p0.pikist.com/photos/117/802/background-isolated-garden-scissors-pruner-shears-secateurs-object-equipment.jpg",
+        size: "small",
+        description: "For pruning and deadheading",
+      },
+      {
+        name: "Bucket",
+        image:
+          "https://images.homedepot-static.com/productImages/db7d4968-169c-4360-aece-5fd544701f00/svn/orange-the-home-depot-paint-buckets-lids-05glhd2-c3_1000.jpg",
+        size: "medium",
+        description: "for collecting debris",
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="sass" scoped>
+.tasks
+  display: flex
+</style>
