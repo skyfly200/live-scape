617e8b20100c1e87e9b54967b5aed268b3a48797
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-16 12:11:01 -0600

    add data in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,41 +1,62 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     tasks: [
       {
         id: 0,
-        title: "Weed and Deadhead",
-        note: "Weed and deadhead the back and side yards",
+        location: 0,
+        title: 'Weed and Deadhead',
+        note: 'Weed and deadhead the back and side yards',
+        tools: [0, 1, 2],
+        images: [],
+      },
+      {
+        id: 1,
+        location: 1,
+        title: 'Weed and Deadhead',
+        note: 'Weed and deadhead the back and side yards',
         tools: [0, 1, 2],
         images: [],
       },
     ],
+    locations: [
+      {
+        customerName: 'Libby Fly',
+        address: '2644 Mapleton Ave. Boulder, CO',
+        notes: 'Close the gate',
+      },
+      {
+        customerName: 'Jerry Mathews',
+        address: '1234 Bluff St. Boulder, CO',
+        notes: "Don't run the blower",
+      },
+    ],
     tools: [
       {
-        name: "Weeding Tool",
-        image: "",
-        size: "small",
-        description: "",
+        name: 'Weeding Tool',
+        image: '',
+        size: 'small',
+        description: '',
       },
       {
-        name: "Hand Pruners",
-        image: "",
-        size: "small",
-        description: "",
+        name: 'Hand Pruners',
+        image: '',
+        size: 'small',
+        description: '',
       },
       {
-        name: "Bucket",
-        image: "",
-        size: "medium",
-        description: "",
+        name: 'Bucket',
+        image: '',
+        size: 'medium',
+        description: '',
       },
     ],
   },
   mutations: {},
   actions: {},
   modules: {},
-});
+})
