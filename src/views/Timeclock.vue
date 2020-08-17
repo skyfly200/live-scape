366a8f38d1b69fc828f73fdc60a148a85c12233b
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-16 18:00:49 -0600

    add timeclock buttion and readout

--- src/views/Timeclock.vue
+++ src/views/Timeclock.vue
@@ -3,24 +3,48 @@
   v-card
     v-card-title Time Clock
     v-card-text
+      h1.my-4 {{ elapsed }}
       v-btn(@click="logTime") {{ running ? 'Clock Out' : 'Clock In' }}
 </template>
 
 <script>
+import { formatDuration, intervalToDuration } from "date-fns";
+
 export default {
   data: () => ({
     running: false,
-    entrtries: [],
+    entries: [],
   }),
+  computed: {
+    elapsed: function() {
+      let count = this.entries.length;
+      if (count === 0) return "00:00:00";
+      return formatDuration(
+        intervalToDuration({
+          start: this.entries[count - 1].time,
+          end: new Date(),
+        })
+      );
+    },
+  },
   methods: {
     logTime() {
+      this.running = !this.running;
       let log = {
         state: this.running,
-        time: Date(),
+        time: new Date(),
       };
-      this.running = !this.running;
-      this.entrtries.push(log);
+      this.entries.push(log);
     },
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.flex
+  display: flex
+  width: 100%
+.center
+  align-items: center
+  text-align: center
+</style>
