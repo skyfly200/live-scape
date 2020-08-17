3bc419d387f2e670056de33dd7a574b038589d6f
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-17 00:32:09 -0600

    jobs view

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -1,7 +1,65 @@
 <template lang="pug">
-.jobs
-  v-card
-    v-card-title This is a job
+v-container.jobs.d-flex
+  v-card.ma-4(v-for="job in jobs")
+    v-card-title {{ job.customerName }}
+    v-card-subtitle {{ job.address }}
     v-card-text
-      p make notes here
+      h3 {{ getTasks(job.id).length }} New Task(s)
+      p {{ job.notes }}
+    v-divider
+    v-card-actions
+      v-spacer
+      v-btn(:to="'/jobs/' + job.id") Job Info
 </template>
+
+<script>
+export default {
+  name: "Tasks",
+  methods: {
+    getTasks: function(index) {
+      return this.tasks.filter((task) => task.job === index);
+    },
+  },
+  data: () => ({
+    tasks: [
+      {
+        id: 0,
+        status: "new",
+        job: 0,
+        title: "Weed and Deadhead",
+        description: "Weed and deadhead the back and side yards",
+        notes: "",
+        tools: [0, 1, 2],
+      },
+      {
+        id: 1,
+        status: "new",
+        job: 1,
+        title: "Plant Suculants",
+        description: "Plant the suculants in the garden",
+        notes: "",
+        tools: [0, 1, 2],
+      },
+    ],
+    jobs: [
+      {
+        id: 0,
+        customerName: "Dan Brown",
+        address: "1203 Spruce St. Boulder, CO",
+        notes: "Close the gate",
+      },
+      {
+        id: 1,
+        customerName: "Jerry Mathews",
+        address: "1234 Bluff St. Boulder, CO",
+        notes: "Don't run the blower",
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="sass" scoped>
+.tasks
+  display: flex
+</style>
