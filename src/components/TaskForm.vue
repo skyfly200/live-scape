de05a1863f57e32b9f406c4bceb76b57640012f1
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-17 23:56:19 -0600

    add a basic task form

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  v-card.task-form
+    v-card-title Add a Task
+    v-card-text
+      v-input(label="Title" v-model="task.title")
+      v-textfield(label="decription" v-model="task.description")
+      v-autocomplete(label="tools" chips v-model="task.tools" :items="tools" item-text="name" item-value="id") 
+    v-card-actions
+      v-btn Clear
+      v-spacer
+      v-btn Add
+</template>
+
+<script>
+export default {
+  name: "TaskForm",
+  data: () => ({
+    task: {
+      id: 3,
+      status: "new",
+      job: 0,
+      title: "Weed and Deadhead",
+      description: "Weed and deadhead the back and side yards",
+      notes: "",
+      tools: [0, 1, 2],
+    },
+    tools: [
+      {
+        id: 0,
+        name: "Weeding Tool",
+        image:
+          "https://www.gardenersedge.com/images/500/4752.jpg?v=100000227430-2",
+        size: "small",
+        description: "For pulling up weeds at the root",
+      },
+      {
+        id: 1,
+        name: "Hand Pruners",
+        image:
+          "https://p0.pikist.com/photos/117/802/background-isolated-garden-scissors-pruner-shears-secateurs-object-equipment.jpg",
+        size: "small",
+        description: "For pruning and deadheading",
+      },
+      {
+        id: 3,
+        name: "Bucket",
+        image:
+          "https://images.homedepot-static.com/productImages/db7d4968-169c-4360-aece-5fd544701f00/svn/orange-the-home-depot-paint-buckets-lids-05glhd2-c3_1000.jpg",
+        size: "medium",
+        description: "for collecting debris",
+      },
+    ],
+  }),
+};
+</script>
