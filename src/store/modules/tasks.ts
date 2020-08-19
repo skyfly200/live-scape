75f573cc7475a93bc251c67d2aa2a48b84a3b712
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-18 18:41:15 -0600

    remove jobs & fix job page

--- src/store/modules/tasks.ts
+++ src/store/modules/tasks.ts
@@ -25,23 +25,30 @@ export default class Tasks extends VuexModule {
     {
       id: 1,
       contact: 1,
+      title: "Libby's",
+      address: "2644 Mapleton Ave. Boulder, CO",
+      notes: ["Libby is awesome"],
+    },
+    {
+      id: 2,
+      contact: 1,
       title: "Jerry's",
       address: "554 Bluff St. Boulder, CO",
       notes: ["Close the gate"],
     },
     {
-      id: 2,
-      contact: null,
+      id: 3,
+      contact: 1,
       title: "Wester Disposal",
       address: "Western Disposal Boulder, CO",
       notes: ["This place is a dump"],
     },
     {
-      id: 3,
-      contact: 3,
-      title: "Libby's",
-      address: "2644 Mapleton Ave. Boulder, CO",
-      notes: ["Libby is awesome"],
+      id: 4,
+      contact: 1,
+      title: "Home Depot",
+      address: "Home Depot Boulder, CO",
+      notes: ["A big box store"],
     },
   ];
   contacts: any = [
--- src/views/Job.vue
+++ src/views/Job.vue
@@ -3,21 +3,22 @@ v-container.job(fluid)
   v-row(no-gutters)
     v-col
       v-card.ma-2(dark)
-          v-card-title {{ contacts[job.contact].lastName }}, {{ contacts[job.contact].firstName }}
-          v-card-subtitle {{ contacts[job.contact].nickname }}
-          v-divider
-          v-card-text.location
-            div
-              h3 {{ locations[job.location].title }}
-              p {{ locations[job.location].address }}
-            div
-              v-btn(:href="'https://www.google.com/maps/dir/?api=1&destination='+ encodeURI(job.address)" target="_blank" color="primary") Navigate To
+        v-img(:src="streetViewURL + encodeURI(location.address)")
+        v-card-title {{ contacts[location.contact].lastName }}, {{ contacts[location.contact].firstName }}
+        v-card-subtitle {{ contacts[location.contact].nickname }}
+        v-divider
+        v-card-text.location
+          div
+            h3 {{ location.title }}
+            p {{ location.address }}
+          div
+            v-btn(:href="'https://www.google.com/maps/dir/?api=1&destination='+ encodeURI(location.address)" target="_blank" color="primary") Navigate To
     v-col
       v-card.ma-2(dark)
           v-card-title Notes
           v-card-text
               v-list 
-                  v-list-item(v-for="note,i in locations[job.location].notes" dense three-line :key="i")
+                  v-list-item(v-for="note,i in location.notes" dense three-line :key="i")
                       v-list-item-content
                           v-list-item-subtitle {{ note }}
   v-row(no-gutters)
@@ -26,11 +27,11 @@ v-container.job(fluid)
         v-card-title Tasks
         v-card-text
           v-list
-            v-list-item(v-for="task in getTasks(job.id)" dense three-line :key="task.id")
+            v-list-item(v-for="task in getTasks(location.id)" dense three-line :key="task.id")
               v-list-item-content
-                v-list-item-title {{ task.title }} for {{ jobs[task.job].name }}
+                v-list-item-title {{ task.title }} for {{ location.name }}
                 v-list-item-subtitle {{ task.description }}
-                v-list-item-subtitle {{ jobs[task.job].address }}
+                v-list-item-subtitle {{ location.address }}
               v-list-item-icon
                 template(v-if="task.status === 'new'")
                   v-icon(color="green" @click="") mdi-play
@@ -57,15 +58,17 @@ export default {
       "locations",
       "contacts",
       "tasks",
-      "jobs",
       "tools",
       "materials",
     ]),
-    job: function() {
-      return this.jobs[this.$route.params.id];
+    location: function() {
+      return this.locations[this.$route.params.id];
     },
   },
-  data: () => ({}),
+  data: () => ({
+    streetViewURL:
+      "https://maps.googleapis.com/maps/api/streetview?size=300x200&fov=80&pitch=0&key=AIzaSyDPbvEAlVbD1oME8UH9b2pbTQJympA5lM8&location=",
+  }),
 };
 </script>
 
