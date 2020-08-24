67a1ffe19f584969e4c0f319fbb98fbc418eeb8a
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-24 08:49:55 -0600

    setup vuexfire

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import { vuexfireMutations } from "vuexfire";
+import { vuexfireMutations, firestoreAction } from "vuexfire";
+import { db } from "./db";
 
 import Auth from "./modules/auth";
 import Timeclock from "./modules/timeclock";
@@ -15,10 +16,17 @@ export default new Vuex.Store({
     timeclock: Timeclock,
     taskSys: Tasks,
   },
-  state: {},
+  state: {
+    todos: [],
+  },
   mutations: {
     ...vuexfireMutations,
   },
-  actions: {},
+  actions: {
+    bindTodos: firestoreAction(({ bindFirestoreRef }) => {
+      // return the promise returned by `bindFirestoreRef`
+      return bindFirestoreRef("todos", db.collection("todos"));
+    }),
+  },
   getters: {},
 });
