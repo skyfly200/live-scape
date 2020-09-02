a60dac37394d2fbfcc5e2814d77af09b372aeda7
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-09-01 23:53:14 -0600

    improve dash cards

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,92 +1,100 @@
 <template lang="pug">
 v-container.dashboard(fluid)
-  v-card.pa-2(dark)
-    v-card-title
-      h1 Dashboard
-    v-container.data-boxes
-      v-divider
-      v-row
-        v-col.data-box
-          h2.data-field {{ timeWorked }}
-          h5 Worked This Week
-        v-divider(vertical)
-        v-col.data-box
-          h1.data-field 3
-          h5 Work Days Left
-        v-divider(vertical)
-        v-col.data-box
-          v-progress-circular.gauge(
-            :value="percentTasks",
-            size="120",
-            color="blue"
-          )
-            span {{ completeTasks }} / {{ taskSys.tasks.length }}
-          h5 Tasks Done
-      v-divider
-  v-card.pa-2.my-3(dark)
-    v-card-title
-      h2 Active Tasks
-    v-card-text
-      v-list
-        v-list-item(
-          v-for="task in activeTasks",
-          dense,
-          three-line,
-          to="/tasks",
-          :key="task.id"
-        )
-          v-list-item-content
-            v-list-item-title {{ task.title }}
-            v-list-item-subtitle {{ task.description }}
-            v-list-item-subtitle {{ task.notes }}
-  v-card.pa-2.my-3(dark)
-    v-card-title
-      h2 Paused Tasks
-    v-card-text
-      v-list
-        v-list-item(
-          v-for="task in pausedTasks",
-          dense,
-          three-line,
-          to="/tasks",
-          :key="task.id"
-        )
-          v-list-item-content
-            v-list-item-title {{ task.title }}
-            v-list-item-subtitle {{ task.description }}
-            v-list-item-subtitle {{ task.notes }}
-  v-card.pa-2.my-3(dark)
-    v-card-title
-      h2 Completed Tasks
-    v-card-text
-      v-list
-        v-list-item(
-          v-for="task in doneTasks",
-          dense,
-          three-line,
-          to="/tasks",
-          :key="task.id"
-        )
-          v-list-item-content
-            v-list-item-title {{ task.title }}
-            v-list-item-subtitle {{ task.description }}
-            v-list-item-subtitle {{ task.notes }}
-  v-card.pa-2(dark)
-    v-card-title
-      h2 New Tasks
-    v-card-text
-      v-list
-        v-list-item(
-          v-for="task in newTasks",
-          dense,
-          three-line,
-          to="/tasks",
-          :key="task.id"
-        )
-          v-list-item-content
-            v-list-item-title {{ task.title }}
-            v-list-item-subtitle {{ task.description }}
-            v-list-item-subtitle {{ task.notes }}
+  v-row
+    v-col
+      v-card.pa-2(dark)
+        v-card-title
+          h1 Dashboard
+        v-container.data-boxes
+          v-divider
+          v-row
+            v-col.data-box
+              h2.data-field {{ timeWorked }}
+              h5 Worked This Week
+            v-divider(vertical)
+            v-col.data-box
+              h1.data-field 3
+              h5 Work Days Left
+            v-divider(vertical)
+            v-col.data-box
+              v-progress-circular.gauge(
+                :value="percentTasks",
+                size="120",
+                color="blue"
+              )
+                span {{ completeTasks }} / {{ taskSys.tasks.length }}
+              h5 Tasks Done
+          v-divider
+  v-row
+    v-col
+      v-card.pa-2.my-3(dark)
+        v-card-title
+          h2 Active Tasks
+        v-card-text
+          v-list
+            v-list-item(
+              v-for="task in activeTasks",
+              dense,
+              three-line,
+              to="/tasks",
+              :key="task.id"
+            )
+              v-list-item-content
+                v-list-item-title {{ task.title }}
+                v-list-item-subtitle {{ task.description }}
+                v-list-item-subtitle {{ task.notes }}
+    v-col
+      v-card.pa-2.my-3(dark)
+        v-card-title
+          h2 Paused Tasks
+        v-card-text
+          v-list
+            v-list-item(
+              v-for="task in pausedTasks",
+              dense,
+              three-line,
+              to="/tasks",
+              :key="task.id"
+            )
+              v-list-item-content
+                v-list-item-title {{ task.title }}
+                v-list-item-subtitle {{ task.description }}
+                v-list-item-subtitle {{ task.notes }}
+  v-row
+    v-col
+      v-card.pa-2(dark)
+        v-card-title
+          h2 New Tasks
+        v-card-text
+          v-list
+            v-list-item(
+              v-for="task in newTasks",
+              dense,
+              three-line,
+              to="/tasks",
+              :key="task.id"
+            )
+              v-list-item-content
+                v-list-item-title {{ task.title }}
+                v-list-item-subtitle {{ task.description }}
+                v-list-item-subtitle {{ task.notes }}
+    v-col
+      v-card.pa-2(dark)
+        v-card-title
+          h2 Completed Tasks
+        v-card-text
+          v-list
+            v-list-item(
+              v-for="task in doneTasks",
+              dense,
+              three-line,
+              to="/tasks",
+              :key="task.id"
+            )
+              v-list-item-content
+                v-list-item-title {{ task.title }}
+                v-list-item-subtitle {{ task.description }}
+                v-list-item-subtitle {{ task.notes }}
 </template>
 
 <script>
