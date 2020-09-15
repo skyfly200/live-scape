fe978f03cbe9b0034f8caefc5539d112a825931b
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-09-15 15:44:22 -0600

    change user cloud fucntions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
-import * as functions from "firebase-functions";
-const nodemailer = require("nodemailer");
+import * as functions from 'firebase-functions'
+const nodemailer = require('nodemailer')
 
-import { User } from "../../src/models/user";
+import { User } from '../../src/models/user'
 
-const admin = require("firebase-admin");
-admin.initializeApp();
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+const db = admin.firestore()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -20,18 +22,18 @@ admin.initializeApp();
 // 2. https://accounts.google.com/DisplayUnlockCaptcha
 // For other types of transports such as Sendgrid see https://nodemailer.com/transports/
 // TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
-const gmailEmail = functions.config().google.email;
-const gmailPassword = functions.config().google.password;
-const mailTransport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
-});
+// const gmailEmail = functions.config().google.email;
+// const gmailPassword = functions.config().google.password;
+// const mailTransport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: gmailEmail,
+//     pass: gmailPassword,
+//   },
+// });
 
 // Your company name to include in the emails
-const APP_NAME = "LiveScape - Earth Accents Landscaping";
+const APP_NAME = 'LiveScape - Earth Accents Landscaping'
 
 // [START sendWelcomeEmail]
 /**
@@ -41,33 +43,29 @@ const APP_NAME = "LiveScape - Earth Accents Landscaping";
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
   // [END onCreateTrigger]
   // [START eventAttributes]
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
+  const email = user.email // The email of the user.
+  const displayName = user.displayName // The display name of the user.
   // [END eventAttributes]
 
   // create profile document
   const profile = new User({
-    uid: user.disabled,
+    uid: user.uid,
     displayName: user.displayName,
     email: user.email,
     phone: user.phoneNumber,
     photoURL: user.photoURL
       ? user.photoURL
-      : "http://lorempixel.com/g/200/200/cats/1",
+      : 'http://lorempixel.com/g/200/200/cats/1',
     providers: [],
-    active: false,
-    lastLogin: user.metadata.lastSignInTime,
-    role: "",
-  });
+    active: true,
+    role: 'new',
+  })
 
-  // Add a new document in collection "users"
-  admin
-    .firestore()
-    .collection("user")
-    .add(profile);
+  // Add a new document in collection "users" with uid key
+  db.collection('user').doc(user.uid).set(profile)
 
-  return sendWelcomeEmail(email, displayName);
-});
+  return true //sendWelcomeEmail(email, displayName)
+})
 // [END sendWelcomeEmail]
 
 // [START sendByeEmail]
@@ -77,49 +75,59 @@ exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 // [START onDeleteTrigger]
 exports.sendByeEmail = functions.auth.user().onDelete((user) => {
   // [END onDeleteTrigger]
-  const email = user.email;
-  const displayName = user.displayName;
+  const email = user.email
+  const displayName = user.displayName
 
-  return sendGoodbyeEmail(email, displayName);
-});
+  db.collection('user').doc(user.uid).update({ active: false })
+
+  return true //sendGoodbyeEmail(email, displayName)
+})
 // [END sendByeEmail]
+
+// Set admin privilege on the user corresponding to uid.
+// admin.auth().setCustomUserClaims(uid, { admin: true }).then(() => {
+//   // The new custom claims will propagate to the user's ID token the
+//   // next time a new one is issued.
+// });
 
 // Sends a welcome email to the given user.
 async function sendWelcomeEmail(
   email: string | undefined,
-  displayName: string | undefined
+  displayName: string | undefined,
 ) {
   const mailOptions = {
     from: `${APP_NAME} <noreply@wayhighradio.com>`,
     to: email,
-    subject: "",
-    text: "",
-  };
+    subject: '',
+    text: '',
+  }
 
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName ||
-    ""}! Welcome to ${APP_NAME}. I hope you will enjoy the platform.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log("New welcome email sent to:", email);
-  return null;
+  mailOptions.subject = `Welcome to ${APP_NAME}!`
+  mailOptions.text = `Hey ${
+    displayName || ''
+  }! Welcome to ${APP_NAME}. I hope you will enjoy the platform.`
+  await mailTransport.sendMail(mailOptions)
+  console.log('New welcome email sent to:', email)
+  return null
 }
 
 // Sends a goodbye email to the given user.
 async function sendGoodbyeEmail(
   email: string | undefined,
-  displayName: string | undefined
+  displayName: string | undefined,
 ) {
   const mailOptions = {
     from: `${APP_NAME} <noreply@wayhighradio.com>`,
     to: email,
-    subject: "",
-    text: "",
-  };
+    subject: '',
+    text: '',
+  }
 
-  mailOptions.subject = `Bye!`;
-  mailOptions.text = `Hey ${displayName ||
-    ""}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log("Account deletion confirmation email sent to:", email);
-  return null;
+  mailOptions.subject = `Bye!`
+  mailOptions.text = `Hey ${
+    displayName || ''
+  }!, We confirm that we have deleted your ${APP_NAME} account.`
+  await mailTransport.sendMail(mailOptions)
+  console.log('Account deletion confirmation email sent to:', email)
+  return null
 }
